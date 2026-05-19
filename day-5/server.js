cce99856467385eaf75f2b7bbd8713cbcfabd550
Anshika Sengar <sengaranshika94@gmail.com>
2026-05-19 17:35:48 +0530

    env file used

--- day-5/server.js
+++ day-5/server.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose")
 
 async function connectToDb() {
     try {
-        await mongoose.connect(
-            "mongodb://anshika:CG2iXEENQ9XnGoTm@ac-ul9tw63-shard-00-00.ercrrny.mongodb.net:27017,ac-ul9tw63-shard-00-01.ercrrny.mongodb.net:27017,ac-ul9tw63-shard-00-02.ercrrny.mongodb.net:27017/day-5?ssl=true&replicaSet=atlas-lv3rca-shard-0&authSource=admin&appName=cohort2"
-        )
+        await mongoose.connect(process.env.MONGO_URI)
+            
         console.log("Connected to DB")
     } catch (err) {
         console.log(err)
