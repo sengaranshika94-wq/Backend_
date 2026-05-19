7e3f657e201c01a1b1af891ae7767ae2ee53fd83
Anshika Sengar <sengaranshika94@gmail.com>
2026-05-19 17:51:04 +0530

    new updates

--- day-6/server.js
+++ day-6/server.js
@@ -1,4 +1,4 @@
-require("dotenv").congif()
+require("dotenv").config()
 const app=require("./src/app");
 const connectToDb=require("./src/config/database")
 connectToDb()
--- day-6/src/app.js
+++ day-6/src/app.js
@@ -9,12 +9,23 @@ app.use(express.json()) //middleware - if we dont write it we cannot get data in
  */
 app.post("/notes",async(req,res)=>{
      const {title,description}= req.body //destructuring getting title and desc. from req.body
-     const notes= await notesModel.create({
+     const note= await notesModel.create({
         title,description
     })
     res.status(201).json({
         message: "Note created successfully",
-        notes
+        note
     })
+})
+/**
+ * -get /notes
+ * -fetch all the notes data 
+ */
+app.get("/notes",async(req,res)=>{
+   const notes= await notesModel.find() //gets all the notes info in the arr of obj format
+   res.status(200).json({
+    message : "notes fetched successfully",
+    notes
+   })
 })
 module.exports=app;
